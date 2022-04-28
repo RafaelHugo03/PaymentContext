b91da9113e33637be1e5fbe6d4a45904da1a9f8d
Rafael Evangelista <Rafaelhugo03@outlook.com>
2022-04-28 18:12:18 -0300

    Shared finalizado

---
+++ PaymentContext.Shared/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Flunt.Notifications;
+
+namespace PaymentContext.Shared.Entities
+{
+    public abstract class Entity : Notifiable
+    {
+        public Guid Id { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
